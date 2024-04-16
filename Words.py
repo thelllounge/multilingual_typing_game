435d6645f2d7f9c8ce3a9c6713535f8411b1d507
thelllounge <46868624+thelllounge@users.noreply.github.com>
2024-04-16 16:57:11 +0900

    Worked on adding a word class

---
+++ Words.py
@@ -0,0 +1,14 @@
+# Maybe making objects instead of words is better than what I was doing before. I can add objects to an array to be populated
+import random
+import pygame
+
+
+class Word:
+    def __init__(self, vocab_list, screen_width, font):
+        # This gets a pair of words from the english-french dictionary
+        # Maybe change to language1 language2 so it can be more than French?
+        self.words = random.choice(list(vocab_list.items()))
+        self.english = self.words[0]
+        self.french = self.words[1]
+        self.wordimg = font.render(self.english, True, (255, 255, 255))
+        # self.position
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import create_and_move_words
 import json
+import Words
 
 # Various constants that don't rely on PyGame being initialized
 SCREEN_WIDTH = 650
@@ -19,6 +20,9 @@ text_font = pygame.font.SysFont("Arial", 30)
 with open("words.json", "r") as vocabulary_list:
     words = json.load(vocabulary_list)
 
+# Array of words that have been created
+words_on_screen = []
+
 # The loop that is the game.
 while running:
     for event in pygame.event.get():
@@ -27,10 +31,20 @@ while running:
 
     screen.fill("black")
 
-    # The function that pulls vocabulary words and puts them on the screen.
-    # Currently, it continuously goes off. I need to figure out how to make it happen at a certain speed.
-    # Maybe add the word to a list of things on the screen so the screen fill doesn't just erase them.
-    create_and_move_words.write_word(screen, words, text_font, (255, 255, 255))
+    # # The function that pulls vocabulary words and puts them on the screen.
+    # # Currently, it continuously goes off. I need to figure out how to make it happen at a certain speed.
+    # # Maybe add the word to a list of things on the screen so the screen fill doesn't just erase them.
+    # # !! Updating this to use objects instead of a function like this. needs lots of work.
+    # create_and_move_words.write_word(screen, words, text_font, (255, 255, 255))
+
+    words_on_screen.append(Words.Word(words, SCREEN_WIDTH, text_font))
+
+    if len(words_on_screen) >= 10:
+        words_on_screen = []
+
+# This works. Need to think about making position part of the object.
+    for word in words_on_screen:
+        screen.blit(word.wordimg, (SCREEN_WIDTH / 2 - word.wordimg.get_width() / 2, 0))
 
     pygame.display.flip()
 

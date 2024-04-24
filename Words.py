9571567fa2570a84024847fbf4aa6650c576886f
thelllounge <46868624+thelllounge@users.noreply.github.com>
2024-04-24 16:00:30 +0900

    Worked on adding a word class

--- Words.py
+++ Words.py
@@ -1,17 +1,18 @@
-# Maybe making objects instead of words is better than what I was doing before. I can add objects to an array to be populated
+# Maybe making objects instead of words is better than what I was doing before. I can add objects to an array to be
+# populated
 import random
-import pygame
 
 
 class Word:
     def __init__(self, vocab_list, screen_width, font):
         # This gets a pair of words from the english-french dictionary
-        # Maybe change to language1 language2 so it can be more than French?
+        # Maybe change to language1 language2, so it can be more than French?
         self.words = random.choice(list(vocab_list.items()))
         self.english = self.words[0]
-        self.french = self.words[1]
-        self.wordimg = font.render(self.english, True, (0, 0, 0))
-        self.position = (random.randint(0, screen_width - self.wordimg.get_width()), 0)
+        self.french = self.words[1][0]
+        self.non_accent = self.words[1][1]
+        self.word_img = font.render(self.english, True, (0, 0, 0))
+        self.position = (random.randint(0, screen_width - self.word_img.get_width()), 0)
 
     def move(self, speed):
         # Moves words down the screen
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame
-import create_and_move_words
 import json
 import Words
 
 # Various constants that don't rely on PyGame being initialized
 SCREEN_WIDTH = 650
 SCREEN_HEIGHT = 780
-FALL_SPEED = 25
+FALL_SPEED = 1
 
 GAME_SPEED = 1000
 TEST_SPEED = 100
-WORD_DROP_RATE = 10
+WORD_DROP_RATE = 5
+ADD_WORD_EVENT = pygame.event.custom_type()
+
 
 answer_text = ""
+correct_answers = 0
 
 # General PyGame things
 pygame.init()
@@ -25,7 +27,7 @@ french_background = pygame.image.load("Paris unfinished.png")
 text_font = pygame.font.SysFont("Arial", 30)
 
 # Opens the vocabulary JSON dictionary up to be readable.
-with open("words.json", "r") as vocabulary_list:
+with open("words.json", "r", encoding="utf-8") as vocabulary_list:
     words = json.load(vocabulary_list)
 
 # Array of words that have been created
@@ -33,20 +35,28 @@ words_on_screen = []
 
 # The loop that is the game.
 while running:
+    pygame.time.set_timer(ADD_WORD_EVENT, 25)
     for event in pygame.event.get():
+        if event.type == ADD_WORD_EVENT:
+            print("event")
+            print(pygame.time.get_ticks())
+            words_on_screen.append(Words.Word(words, SCREEN_WIDTH, text_font))
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
-                answer_text = answer_text [:-1]
+                answer_text = answer_text[:-1]
             elif event.key == pygame.K_RETURN:
                 answer_text = ""
+            # I need to figure out a way to make sure I can click buttons to do accents without fucking up the answer
+            elif event.key == pygame.K_6:
+                pass
             else:
-
                 answer_text += event.unicode
         for word in words_on_screen:
-            if answer_text == word.french:
+            if answer_text == word.non_accent:
                 answer_text = ""
+                correct_answers += 1
                 words_on_screen.pop(words_on_screen.index(word))
 
     screen.blit(french_background, (0, 0))
@@ -54,23 +64,33 @@ while running:
     # This puts the answer text on the screen
     answer_text_img = text_font.render(answer_text, True, (0, 0, 0))
     screen.blit(answer_text_img, (110, 635))
+    score_img = text_font.render(f"Score: {correct_answers}", True, (255, 255, 255))
+    screen.blit(score_img, (SCREEN_WIDTH / 2 - score_img.get_width() / 2, 715))
 
     # This pulls words from the dictionary and adds them to the list to drop.
     # Maybe I can use pygame.time.set_timer() for this. I need to learn how that works.
-    if int(pygame.time.get_ticks()/1000) % WORD_DROP_RATE == 0:
-        words_on_screen.append(Words.Word(words, SCREEN_WIDTH, text_font))
+
+    # def add_word(array, vocabulary, width, font):
+    #     array.append(Words.Word(vocabulary, width, font))
+
+    # do_it = add_word(words_on_screen, words, SCREEN_WIDTH, text_font)
+
+
+
+    # if int(pygame.time.get_ticks()/1000) % WORD_DROP_RATE == 0:
+    #     words_on_screen.append(Words.Word(words, SCREEN_WIDTH, text_font))
 
 # This works. Need to think about making position part of the object.
     for word in words_on_screen:
-        screen.blit(word.wordimg, word.position)
+        screen.blit(word.word_img, word.position)
         # If a word hits the bottom of the play field game is over
-        # if word.position[1] >= 540:
-        #     running = False
+        if word.position[1] >= 540:
+            running = False
         word.move(FALL_SPEED)
 
     pygame.display.flip()
 
-    pygame.time.wait(GAME_SPEED)
+    # pygame.time.wait(TEST_SPEED)
 
     clock.tick(60)
 
